27ed83caaf3b08702155c8e9a793f0da221e2f80
O_yao.dong <YaoDong@wistronits.com>
2024-01-25 18:53:32 +0800

    feat: 测试接口

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -5,6 +5,23 @@
   </div>
 </template>
 <script lang="ts" setup>
+import axios from "axios";
+axios
+  .post("https://152.136.150.189/reactAdmin/authorityList", { a:1})
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+axios
+  .get("https://152.136.150.189/public/getSlider")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const aaa = () => {
   console.log(222);
 };
